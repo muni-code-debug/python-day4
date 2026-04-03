83ae047261a4b2dbcc85ed65f1aab74171c4bd6c
Kasetty Muni <munisaikasetty@gmail.com>
2026-04-03 08:58:44 +0530

    Add files via upload

---
+++ python day4.py
@@ -0,0 +1,103 @@
+"""#input reading from user#
+
+name = input()
+print(type(name), name)
+
+#reading integers from user
+num = int(input())
+print(type(num), num)
+
+#reading float value from user
+value = float(input())
+print(type(value), value)
+
+x=int(input())
+y=int(input())
+c=complex(x,y)
+print(type(c),c)
+
+n=bool(input())
+print(type(n),n)
+
+n=bool(int(input()))
+print(type(n),n)
+
+
+#one egg price is 5 rupees and what is price of 'n'eggs price
+n=int(input())
+oneegg=5
+c=oneegg*n
+print(c)"""
+
+
+#if box contains N pencils and total price is M, then what price of each pencil price
+"""n=int(input())
+m=int(input())
+z=m/n
+print(z)"""
+
+
+"""red ball price is-25
+    green ball price is-30
+    if i buy N-Red ball and M-Green balls, than what is the total cost."""
+n=int(input())
+m=int(input())
+redball=25
+greenball=30
+Z=n*redball+m*greenball
+print("the total balls cost is",Z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
