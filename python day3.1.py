d6bea8213b7e085a7a407364a4094f646e6277d8
Kasetty Muni <munisaikasetty@gmail.com>
2026-04-03 08:58:02 +0530

    Add files via upload

---
+++ python day3.1.py
@@ -0,0 +1,67 @@
+"""check number is zero or non zero
+ a=0
+if a==0:
+    print("it is zero")
+else:
+    print("it is non-zero")"""
+
+"""2.check number is +ve
+a=0
+if a>-1:
+    print("it is positive")"""
+
+#check number is even or odd#
+"""a=2
+n=int(input())
+if n%a==0:
+    print("it is even number")
+else:
+    print("it is odd number")"""
+    
+#4.check weather the person is eligible for vote or not
+"""voteage =18
+if voteage>=18:
+    print("he is eligible for vote")
+else:
+    print("he is not eligible for vote")"""
+    
+#5.check weather the number is divisible with both 3 and 5 or not
+
+"""n=int(input())
+if n%3==0 and n%5==0:
+    print("it is divisible")
+else:
+    print("it is not divisible")"""
+  
+#6.find biger number among two numbers
+
+m=int(input())
+n=int(input())
+if m>n and n>o:
+    print("the bigger number",m)
+else:
+    print("the bigger number",n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
